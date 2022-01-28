402336ca17303cda0195a6972063162b8b6b1049
renato3x <pereirarenato21@gmail.com>
2022-01-28 13:23:31 -0300

    Payment, one-to-one association

--- src/main/java/com/website/course/config/TestConfig.java
+++ src/main/java/com/website/course/config/TestConfig.java
@@ -95,6 +95,11 @@ public class TestConfig implements CommandLineRunner {
 
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+
+    orderRepository.save(o1);
+
     /*
      * o método saveAll() salva várias entidades de um banco de dados de uma vez
      * */
--- src/main/java/com/website/course/entities/Order.java
+++ src/main/java/com/website/course/entities/Order.java
@@ -35,6 +35,9 @@ public class Order implements Serializable {
   @OneToMany(mappedBy = "id.order")
   private Set<OrderItem> items = new HashSet<>();
 
+  @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+  private Payment payment;
+
   public Order() {}
 
   public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -80,6 +83,14 @@ public class Order implements Serializable {
 
   public Set<OrderItem> getItems() {
     return items;
+  }
+
+  public Payment getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Payment payment) {
+    this.payment = payment;
   }
 
   @Override
